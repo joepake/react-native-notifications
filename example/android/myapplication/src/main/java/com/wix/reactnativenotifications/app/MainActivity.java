bf8c4d1faaa4c6244daa3bcb15cdd7afe22ea930
ThucNT <thucnt@daivietgroup.net.vn>
2020-08-19 14:09:19 +0700

    init

---
+++ example/android/myapplication/src/main/java/com/wix/reactnativenotifications/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.wix.reactnativenotifications.app;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "NotificationsExampleApp";
+    }
+}
